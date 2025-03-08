ac64fc2266995ea891218b2d01f57d98cf6bbdce
Joshua Balloch <joshuaballoch@gmail.com>
2025-03-08 21:51:02 +0000

    Reorder dock and add dock location line

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/System/Applications/Mail.app"
-dockutil --no-restart --add "/System/Applications/Calendar.app"
-dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
-dockutil --no-restart --add "/System/Applications/System Settings.app"
+dockutil --no-restart --add "/Applications/Notion.app"
+dockutil --no-restart --add "/Applications/Notion Calendar.app"
+dockutil --no-restart --add "/Applications/zoom.us.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/System/Applications/System Settings.app"
+
+defaults write com.apple.dock orientation left
 
 killall Dock
