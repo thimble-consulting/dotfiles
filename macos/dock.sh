72d8f9fbbd73105f76f84e8c77aefbbdcc06cd6d
Joshua Balloch <joshuaballoch@gmail.com>
2025-03-10 09:29:04 +0000

    Add messages and firefox to dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
+dockutil --no-restart --add "/System/Applications/Messages.app"
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Notion.app"
 dockutil --no-restart --add "/Applications/Notion Calendar.app"
