aa8de5331cc944537b88f2f23dd760ef7f457838
Joshua Balloch <joshuaballoch@gmail.com>
2025-05-07 13:45:12 +0100

    Remove branch from lualine:
    * Just makes it too long to be able to see meaningful filenames

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -23,7 +23,7 @@ return {
     },
     sections = {
       lualine_a = {'mode'},
-      lualine_b = {'branch', 'diff', 'diagnostics'},
+      lualine_b = {'diff', 'diagnostics'},
       lualine_c = { { 'filename', path = 1 }},
       lualine_x = {'encoding', 'fileformat', 'filetype'},
       lualine_y = {'progress'},
